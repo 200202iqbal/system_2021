e51941a748ecc39789e48afbda12914aa4a3d323
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2022-02-28 10:31:59 +0900

    Test

---
+++ KimatsuShiken.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Muhammad Iqbal Tawakal">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+        body{
+            background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(8,8,111,1) 5%, rgba(9,9,121,1) 30%, rgba(0,212,255,1) 100%);
+        color:white;
+        }
+        
+
+    </style>
+  </head>
+  <body>
+    
+    
+   
+    <div class="container">
+  <div class="row">
+    <div class="col">
+    <h1>Hello, world!</h1>
+    </div>
+    <div class="col-7">
+    <h2>氏名：ムハンマドイクバルタワカル</h2>
+    <h2>学籍番号:2002020</h2>
+    <h2>コース:IT-ゲームソフト科</h2>
+    </div>
+    <div class="col">
+    <?php
+        echo "Hello,World";
+
+    ?>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
